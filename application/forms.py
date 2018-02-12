254e8d2007dd8c7a9b2ab2a65149b7ab9630c4a6
james947 <jmuriuki947@gmail.com>
2018-02-12 06:00:10 -0800

    Add class Login form

--- application/forms.py
+++ application/forms.py
@@ -7,15 +7,13 @@ class SignupForm(FlaskForm):
     
     #firstname variable
     firtsname = StringField('firstname',[
-
         validators.data_required(),
         validators.length(min=4 ,max=10)
 
     ] )
 
     #lastname variable
-    lastnamename = StringField('lastname',[
-
+    lastname = StringField('lastname',[
         validators.data_required(),
         validators.length(min=4 ,max=10)
 
@@ -23,7 +21,6 @@ class SignupForm(FlaskForm):
 
     #username variable
     username = StringField('username',[
-
         validators.data_required(),
         validators.length(min=4 ,max=10)
 
@@ -31,12 +28,34 @@ class SignupForm(FlaskForm):
 
     #emailvariable
     email = StringField('email',[
-
         validators.data_required(),
         validators.length(min=4 ,max=10),
-                    validators.regexp(
-                r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)",
+        validators.regexp(r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)",
                 flags=0, message="Invalid email address"
             )
 
     ] )
+
+    #password Variable
+    Password = PasswordField('password', [
+        validators.data_required(),
+        validators.length(min=8, message="The minimun required lenght is 8"),
+    ])
+
+    #submit variable
+    submit = SubmitField("submit")
+
+#class to validate login
+class LoginForm(FlaskForm):
+ #auhenticate email
+    email = StringField('email', [
+        validators.data_required(),
+        validators.email(message="invalid email adress"),
+        validators.regexp( r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)", message="Invalid email address"
+        )  
+    ])
+
+  #auth password
+    password =PasswordField('password',[validators.data_required()])
+    #pass login
+    login = SubmitField('login')
