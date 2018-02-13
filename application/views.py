a887a120ed661beaae7e6e65eb923875c2c5e4f4
james947 <jmuriuki947@gmail.com>
2018-02-12 23:36:04 -0800

    [ADD] secret key and post method

--- application/views.py
+++ application/views.py
@@ -1,7 +1,17 @@
-from flask import Flask, render_template, redirect ,url_for
+#importing methods
+from flask import Flask, render_template, redirect ,url_for, request
+from .forms import SignupForm, LoginForm
+from .users import Users, USERS
 
+#instanciate app
 app = Flask(__name__)
-
-@app.route('/index')
+#secret key 
+app.config['SECRET_KEY'] ="b'6b3105e553c75d921da76d0b13344700cf3b1b8e98efb639'"
+#route to index page
+@app.route('/index', methods=['GET','POST'])
 def index():
-    return render_template("index.html")
+    form = LoginForm(request.form)
+    if request.method == "POST" and form.validate():
+        
+
+    return render_template("index.html",form=form)
