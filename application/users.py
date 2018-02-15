41e6a2bab77a094a3a153f0160b699e3499d6738
james947 <jmuriuki947@gmail.com>
2018-02-14 20:47:24 -0800

    Fixed indentation error

--- application/users.py
+++ application/users.py
@@ -4,26 +4,25 @@ USERS = {}
 
 class Users(object):
     #Create a constructor with user args
-    def __init__(self, email=None, username=None, Password=None):
+    def __init__(self, email=None, username=None, password=None):
 
         self.email = email
         self.username = username
-        self.Password = Password
+        self.password = password
 
-        def add_user(self,email,username,password):
-            #add users to the app
-            if email not in USERS:
-                USERS[email] = {"email":email, "username":username, "password":password}
-                return "user added successfully"
-            return "Sorry, Email already registered"
+    def add_user(self,email, username, password):
+        #add users to the app
+        if email not in USERS:
+            USERS[email] = {"email":email, "username":username, "password":password}
+            return "user added successfully"
+        return "Sorry, Email already registered"
 
-
-        def get_user (self,email,password):
-            #get user from the list
-            if email in USERS:
-                current_user = USERS[email]
-                #authenticate password
-                if current_user["password"] == Password:
-                    return current_user
-                return "password error"
-            return "users not found"    
+    def get_user (self, email, password):
+        #get user from the list
+        if email in USERS:
+            current_user = USERS[email]
+            #authenticate password
+            if current_user["password"] == password:
+                return current_user
+            return "password error"
+        return "users not found"    
